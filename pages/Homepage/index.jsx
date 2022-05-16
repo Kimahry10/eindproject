9660b60e844b35c07c58f8fb7541f9e8e5475d1e
Dieter Schepers <dieter.schepers@student.arteveldehs.be>
2022-05-16 18:13:09 +0200

    init

---
+++ pages/Homepage/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Title, Title2 } from './styles'
+
+const Homepage = () => {
+  return (
+    <>
+      <Title>Homepage</Title>
+      <Title2>Homepage</Title2>
+    </>
+  )
+}
+
+export default Homepage
---
+++ pages/Homepage/styles/index.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+const Title2 = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: green;
+`;
+export { Title, Title2 }
