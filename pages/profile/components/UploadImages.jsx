e5260fa0eba37c65b02c8f3d2d556c6bb0ccbf5c
pgm-dieterschepers <dieter.schepers@student.arteveldehs.be>
2022-05-29 17:26:26 +0200

    user can now upload image and it gets diplayed in the frontend

--- pages/profile/components/UploadImages.jsx
+++ pages/profile/components/UploadImages.jsx
@@ -1,20 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FileUpload } from 'primereact/fileupload';
+import Image from 'next/image'
 
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
 
 const UploadImages = () => {
+  const axios = require('axios')
+  const API_ENDPOINT = "https://18yb3mgnpj.execute-api.eu-west-3.amazonaws.com/default/getPreSignedImageURL"
 
-  const myUploader = (event) => {
-    //event.files == files to upload
-    console.log(event.files)
+  const [img, setImg] = useState()
+
+  const handleSubmit = async (event) => {
+    const files = event.files
+    if (files.length > 1) {
+      alert('only 1 file can be uploaded')
+    } else {
+      // presigned URL
+      const response = await axios({
+        method: 'GET',
+        headers: {
+          "Content-Type": "images/jpeg"
+        },
+        url: API_ENDPOINT
+      })
+      // upload file to S3
+      const result = await fetch(response.data.uploadURL, {
+        method: 'PUT',
+        body: files[0]
+      })
+      console.log(result)
+      // image
+      setImg(result.url.split('?')[0])
+
+      // if (result.status === 200) {
+      //   alert('file uploaded')
+      // }
+    }
   }
+  console.log(img)
+
+
 
   return (
     <>
-      <FileUpload customUpload uploadHandler={myUploader} name="demo[]" url="./upload" multiple maxFileSize="10000000" accept="image/*" />
+      <FileUpload customUpload uploadHandler={handleSubmit} name="demo[]" url="./upload" multiple maxFileSize="10000000" accept="image/*" />
+      {img && <Image src={img} height={300} width={300} alt='icon' />}
     </>
   )
 }
